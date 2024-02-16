708215efaf8cdf0e865099b8c50686e2c1ddfcf3
WUOTE <106106310+WUOTE@users.noreply.github.com>
2024-02-16 12:36:05 +0600

    Fix info message text which pops up after copying coordinates

--- public/js/script.js
+++ public/js/script.js
@@ -209,7 +209,7 @@ const appendAlert = (message, type) => {
 const alertTrigger = document.getElementById('liveAlertBtn')
 if (alertTrigger) {
 	alertTrigger.addEventListener('click', () => {
-		appendAlert('Nice, you triggered this alert message!', 'success')
+		appendAlert('Copied current map coordinates to your clipboard!', 'success')
 	})
 }
 
