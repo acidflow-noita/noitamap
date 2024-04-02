5da3e4133c5803c032084db6e0704697df08e5b7
WUOTE <106106310+WUOTE@users.noreply.github.com>
2024-04-02 14:23:08 +0600

    Add bosses data structure

---
+++ public/js/bosses.js
@@ -0,0 +1,108 @@
+let noitaBosses = {
+  bosses: [
+    {
+      name: ["Pyramid Boss", "Kolmisilmän Koipi", "Three-Eye's Legs"],
+      spawns: "Pyramid",
+      mapCoordinate:
+        "https://map.runfast.stream/?map=regular&x=9984&y=-786&zoom=823",
+    },
+    {
+      name: ["Leviathan", "Syväolento", "Creature of the Deep"],
+      spawns: "Lake",
+      mapCoordinate:
+        "https://map.runfast.stream/?map=regular&x=-14024&y=9994&zoom=987",
+    },
+    {
+      name: [
+        "Door boss",
+        "Veska",
+        "Molari",
+        "Mokke",
+        "Seula",
+        "Gate Guardian",
+        "Triangle boss",
+      ],
+      spawns: "Temple of The Art",
+      mapCoordinate:
+        "https://map.runfast.stream/?map=regular&x=2837&y=11562&zoom=812",
+    },
+    {
+      name: ["Dragon", "Suomuhauki", "Scale Pike"],
+      spawns: "Dragoncave",
+      mapCoordinate:
+        "https://map.runfast.stream/?map=regular&x=2347&y=7444&zoom=917",
+    },
+    {
+      name: ["Tiny", "Limatoukka", "Slime Maggot", "Slime Caterpillar"],
+      spawns: "Buried skull",
+      mapCoordinate:
+        "https://map.runfast.stream/?map=regular&x=14904&y=16428&zoom=1022",
+    },
+    {
+      name: ["Meat Boss", "Kolmisilmän sydän", "Three-Eye's Heart"],
+      spawns: "Meat Realm",
+      mapCoordinate:
+        "https://map.runfast.stream/?map=regular&x=6667&y=8448&zoom=770",
+    },
+    {
+      name: ["Alchemist Boss", "Ylialkemisti", "High Alchemist"],
+      spawns: "Abandoned Alchemy Lab",
+      mapCoordinate:
+        "https://map.runfast.stream/?map=regular&x=-4840&y=851&zoom=796",
+    },
+    {
+      name: ["Kolmi", "Kolmisilmä", "Three-Eye"],
+      spawns: "The Laboratory",
+      mapCoordinate:
+        "https://map.runfast.stream/?map=regular&x=3556&y=13026&zoom=849",
+    },
+    {
+      name: ["Mecha Kolmi", "Kolmisilmän silmä", "Three-Eye's Eye"],
+      spawns: "Power Plant",
+      mapCoordinate:
+        "https://map.runfast.stream/?map=regular&x=13987&y=11123&zoom=875",
+    },
+    {
+      name: ["Friend Boss", "Toveri", "Friend"],
+      spawns: "Friend Room",
+      mapCoordinate:
+        "https://map.runfast.stream/?map=regular&x=25360&y=4341&zoom=917",
+    },
+    {
+      name: [
+        "The Master of Masters",
+        "Mestarien mestari",
+        "Grand Master",
+        "Wizard Boss",
+      ],
+      spawns: "Throne Room",
+      mapCoordinate:
+        "https://map.runfast.stream/?map=regular&x=12573&y=15178&zoom=796",
+    },
+    {
+      name: ["The Forgotten", "Unohdettu", "Ghost Boss"],
+      spawns: "Forgotten Cave",
+      mapCoordinate:
+        "https://map.runfast.stream/?map=regular&x=-11515&y=13123&zoom=744",
+    },
+    {
+      name: [
+        "Bridge Boss",
+        "Sauvojen tuntija",
+        "Connoisseur of Wands",
+        "Squid Boss",
+        "Pit Boss",
+        "Wand Boss",
+      ],
+      spawns: "Lava Lake",
+      mapCoordinate:
+        "https://map.runfast.stream/?map=regular&x=4165&y=889&zoom=970",
+    },
+    {
+      name: ["Deer Boss", "Tapion vasalli", "Tapio's Vassal", "Island Boss"],
+      spawns: "Lake Island",
+      mapCoordinate:
+        "https://map.runfast.stream/?map=regular&x=-13670&y=134&zoom=796",
+    },
+  ],
+};
